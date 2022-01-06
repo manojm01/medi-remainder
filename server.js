6abcf7187691e7da9a3a00e482cd399321cc93dc
manojm01 <manojmetgud035@gmail.com>
2022-01-06 14:29:29 +0530

    static file issue resolved

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const userController = require('./server/controllers/userController');
 
 
 const app = express();
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("assets"));
