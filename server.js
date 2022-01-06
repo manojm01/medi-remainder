7dd36c3d8c8400c70a1c89bf01eee4cd8c5a25d4
Ashokav008 <ashokwavre.wce@gmail.com>
2022-01-06 10:58:37 +0530

    changing index.ejs

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const ejs = require("ejs");
+const path = require('path');
 const bodyParser = require("body-parser");
-
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("assets"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use('/static', express.static(path.join(__dirname, 'assets')))
 
+const router = require("./router")
 
-app.get("/",(req,res)=>{
-    res.render("register")
+app.get("/", (req, res) => {
+    res.render("index")
 })
 
-app.listen(3000,()=>{
-    console.log("Server is ready");
+// app.use('/'.router);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 })
