cbaadf99992e06a97632b04b0c1296d778067e10
Ashokav008 <ashokwavre.wce@gmail.com>
2022-01-08 03:38:11 +0530

    get request to the database function added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,6 +4,30 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Medicine = mongoose.model('Medicine');
 
+// -----------------------------------------------
+// -----------------------------------------------
+router.get('/set', function(req, res, next) {
+
+    username = "Ashoka" //this username will replace the actual username of the logged in user..
+    Medicine.find({ user_name: username })
+        .then(data => {
+            if (!data) {
+                console.log('Failed to retrieve the Course List: ' + err);
+            } else {
+                console.log(data)
+                res.render("set", {
+                    userData: data
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: "Erro retrieving user with id " + email })
+        })
+
+});
+
+// --------------------------------------------------
+// --------------------------------------------------
 
 
 router.get('/', (req, res) => {
@@ -20,22 +44,20 @@ router.post('/set', (req, res) => {
     user.name = req.body.name;
 
 
+    console.log(req.body);
 
     var medicine = new Medicine();
+    medicine.user_name = "Ashoka";
     medicine.medi_name = req.body.medi_name;
     medicine.morning = req.body.morning;
     medicine.afternoon = req.body.afternoon;
     medicine.night = req.body.night;
-    console.log(req.body);
     // console.log(medicine.medi_name);
-    console.log(req.body.morning);
-    console.log(req.body.afternoon);
-    console.log(req.body.night);
 
     medicine.save()
-        .then(data => { res.render('index') })
+        .then(data => { res.render('set') })
         .catch(err => { console.log(err); })
-    b5b57bd1d230cc81ea5173b2947adc02f618ac5a
+        // b5b57bd1d230cc81ea5173b2947adc02f618ac5a
 
 });
 router.get('/login', (req, res) => {
